b584b7eedf7b70749ffd6cc034c63e60a81c2937
pedro-drosa <pedro_drosa@hotmail.com>
2020-01-28 14:49:35 -0300

    (created route) list all projects

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ server.use(express.json());
 const projects = [];
 
 //Cria um novo projeto
-server.post("/projects", (req, res)=> {
+server.post("/projects", (req, res) => 
+{
     const {id, title} = req.body;
     const project = {id, title, tasks: []};
     projects.push(project);
     return res.json(project);
+});
+
+//Lista todos os projetos
+server.get("/projects", (req, res) =>
+{
+    return res.json(projects);
 });
 
 server.listen(3000);
