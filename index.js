44c933b72245688e6df131368ee44a610671b813
pedro-drosa <pedro_drosa@hotmail.com>
2020-01-28 15:01:07 -0300

    (create route) change project

--- index.js
+++ index.js
@@ -18,5 +18,13 @@ server.get("/projects", (req, res) =>
 {
     return res.json(projects);
 });
-
+//Altera o titulo do projeto
+server.put("/projects/:id", (req, res) =>
+{
+    const {id} = req.params;
+    const {title} = req.body;
+    const project = projects.find(obj => obj.id == id);
+    project.title = title
+    return res.json(project);
+});
 server.listen(3000);
